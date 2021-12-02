7b154075b38af87aa63a46dd681a6549c5149c9d
Takahiro Watanabe <treflebonbon@treflebonbon.local>
2021-12-03 03:40:01 +0900

    refactor: mac対応

--- install.sh
+++ install.sh
@@ -11,13 +11,23 @@ linux-gnu)
   # GNU/Linux
   sudo apt update
   sudo apt upgrade -y
-  sudo apt install python3-pip python-is-python3 -y
+  sudo apt install build-essential procps curl file git -y
   ;;
 esac
 
-pip3 install "ansible-lint[community,yamllint]"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-export PATH="$HOME/.local/bin:$PATH"
+case "$OSTYPE" in
+drawin*)
+  # Mac OSX
+  ;;
+linux-gnu)
+  # GNU/Linux
+  export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
+  ;;
+esac
+
+brew install ansible-lint --force
 
 ansible-galaxy collection install community.general
 ansible-playbook "$DIR"/site.yml
